0474c9264f07ae1abe0b8051688df2297dfd703d
ChristoferFernandes <67238384+ChristoferFernandes@users.noreply.github.com>
2022-10-07 22:27:41 -0300

    update

--- 001_Curso_Loiane_Groner/curso-java-basico/idea_intellij/src/exercicios/aula131211/Exercicio11.java
+++ 001_Curso_Loiane_Groner/curso-java-basico/idea_intellij/src/exercicios/aula131211/Exercicio11.java
@@ -1,4 +1,11 @@
 package src.exercicios.aula131211;
 
+import java.util.Scanner;
+
 public class Exercicio11 {
+    public static void main(String[] args) {
+        Scanner tecla = new Scanner(System.in);
+
+
+    }
 }
